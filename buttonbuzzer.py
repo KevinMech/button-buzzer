d6a736b0d9214e9c3f63dcc712b9572226b2a4dc
Kevin <kevin.mellachew@gmail.com>
2017-02-01 17:57:22 -0500

    Added exception that will clean up the pins if script is interrupted prematurely

--- buttonbuzzer.py
+++ buttonbuzzer.py
@@ -5,10 +5,14 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(16, GPIO.IN, GPIO.PUD_UP)
 GPIO.setup(11, GPIO.OUT)
 
-buzz = GPIO.PWM(11, 300)
-while(True):
-    if GPIO.input(16) == 0:
-        buzz.start(1)
-    else:
-        buzz.stop(0)
-    time.sleep(0.1)
+try:
+    buzz = GPIO.PWM(11, 300)
+    while(True):
+        if GPIO.input(16) == 0:
+            buzz.start(1)
+        else:
+            buzz.stop(0)
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    print('GPIO pins cleaned up successfully')
